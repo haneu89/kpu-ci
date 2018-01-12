32fd89c43472ceff7391efb5057ee1d24d47c4ff
jinhyung kim <haneu89@gmail.com>
2018-01-12 16:23:18 +0900

    board 컨트롤러에 board 데이터베이스 사용 선언

--- application/controllers/Board.php
+++ application/controllers/Board.php
@@ -2,6 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Board extends CI_Controller {
+    function __construct() {
+        parent::__construct();  // 부모에 있는 생성자를 호출
+
+        $this->load->database();
+        $this->load->model('board_model');
+    }
 
 	public function index()
 	{
